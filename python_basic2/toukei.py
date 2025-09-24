9489004ff239664711e75645b9fb2cc83a4057f0
azusafurudate <azusafurudate@gmail.com>
2025-09-24 23:14:30 +0900

    tokei.py

---
+++ python_basic2/toukei.py
@@ -0,0 +1,45 @@
+# スペース区切りで入力された整数群において、以下の4つの統計量を計算アプリを実装してください
+# ただし、計算用の組み込み関数やライブラリは使わないこと(sum()やnp.mean()などはNG print()はOK)
+# 1つの統計量につき、それ専用の関数を実装すること
+# 合計値(total)
+def total(numbers):
+    value = 0
+    for n in numbers:
+        value = value + n
+    return value
+
+# 最大値(max)
+def max(numbers):
+    maximum = numbers[0]
+    for n in numbers:
+        if n > maximum:
+            maximum = n
+    return maximum
+
+# 最大値(max)
+def min(numbers):
+    minimum = numbers[0]
+    for n in numbers:
+        if n < minimum:
+            minimum = n
+    return minimum
+
+# 平均値(average)
+def average(numbers):
+    value = total(numbers)
+    count = 0
+    for _ in numbers:
+        count = count + 1
+    return value // count
+
+
+data = input("Data (space separated) > ")
+
+numbers = []
+for text in data.split():
+    numbers.append(int(text))
+
+print("total:", total(numbers))
+print("max:", max(numbers))
+print("min:", min(numbers))
+print("average:", average(numbers))
