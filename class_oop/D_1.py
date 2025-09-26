ac063fb7336ff4aa4a87fe716a087f2d5eed4746
azusafurudate <azusafurudate@gmail.com>
2025-09-26 23:15:01 +0900

    D_1.py修正

--- class_oop/D_1.py
+++ class_oop/D_1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 # 課題D-1: 円オブジェクト
 # 次のコードが正しく動作するような Circle クラスを実装してください
 # area は面積、 perimeter は周囲長(=円周の長さ) という意味です。
@@ -7,21 +9,17 @@ class Circle:
         self.radius = radius
 
     def area(self) -> float:
-        return 3.14 * (self.radius**2)
+        return float(np.pi) * (self.radius**2)
 
     def perimeter(self) -> float:
-        return 2 * 3.14 * self.radius
-
-
-if __name__ == "__main__":
-    print("Hello")  
+        return 2 * float(np.pi) * self.radius
 
 # 半径1の円
 circle1 = Circle(radius=1)
-print(circle1.area())  # 3.14
-print(circle1.perimeter())  # 6.28
+print(round(circle1.area(), 2))  # 3.14
+print(round(circle1.perimeter(), 2))  # 6.28
 
 # 半径3の円
 circle3 = Circle(radius=3)
-print(circle3.area())  # 28.27
-print(circle3.perimeter())  # 18.85
+print(round(circle3.area(), 2))  # 28.27
+print(round(circle3.perimeter(), 2))  # 18.85
