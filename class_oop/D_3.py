b8b2f7df0ed1929e1aa5a520e1f372a6aefdfce0
azusafurudate <azusafurudate@gmail.com>
2025-09-26 23:34:47 +0900

    D_3.py修正

--- class_oop/D_3.py
+++ class_oop/D_3.py
@@ -6,21 +6,21 @@ import math
 
 
 class Square:
-    def __init__(self, side: float) -> None:
+    def __init__(self, side):
         self.side = side
 
-    def area(self) -> float:
-        return self.side * self.side
+    def area(self):
+        return "{:.2f}".format(self.side * self.side)
 
-    def diagonal(self) -> float:
-        return math.sqrt(2) * self.side
+    def diagonal(self):
+        return "{:.2f}".format(math.sqrt(2) * self.side)
 
 
 if __name__ == "__main__":
     square1 = Square(side=1.5)
-    print(f"{square1.area():.2f}")  # 2.25
-    print(f"{square1.diagonal():.2f}")  # 2.12
+    print(square1.area())  # 2.25
+    print(square1.diagonal())  # 2.12
 
     square2 = Square(side=15)
-    print(f"{square2.area():.2f}")  # 225.00
-    print(f"{square2.diagonal():.2f}")  # 21.21
+    print(square2.area())  # 225.00
+    print(square2.diagonal())  # 21.21
